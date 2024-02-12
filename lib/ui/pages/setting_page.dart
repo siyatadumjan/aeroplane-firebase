a1440ede70642829f4333aca794ed55982db4ad1
siyata dumjan <siyatadumjan098@gmail.com>
2024-02-12 20:24:43 +0545

    added a setting page and sign in page

---
+++ lib/ui/pages/setting_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:aeroplane/cubit/auth_cubit.dart';
+import 'package:aeroplane/cubit/page_cubit.dart';
+import 'package:aeroplane/shared/theme.dart';
+import 'package:aeroplane/ui/widgets/custom_button.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthFailed) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(backgroundColor: kRedColor, content: Text(state.error)));
+        } else if (state is AuthInitial) {
+          context.read<PageCubit>().setPage(0);
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/sign-in', (route) => false);
+        }
+      },
+      builder: (context, state) {
+        if (state is AuthLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Center(
+            child: CustomButton(
+                title: 'Sign Out',
+                onPressed: () {
+                  context.read<AuthCubit>().signOut();
+                }),
+          ),
+        );
+      },
+    );
+  }
+}
